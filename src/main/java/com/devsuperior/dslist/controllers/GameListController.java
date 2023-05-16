e3051c73bdde6062967aadd2e3113fcac2849482
abreuermelinda <ermelinda.ti05n@gmail.com>
2023-05-15 21:06:03 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//O controller vai ser a porta de entrada do backend dsiponibilizando a API implementando essa API
+// e expondo essa API para o mundo externo.
+@RestController //Configura a classe para ser um Controller
+@RequestMapping(value= "/lists") //Configura o caminho que vai ser respondido na API
+public class GameListController {
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+}
